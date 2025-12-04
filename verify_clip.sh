359385dec8892e506a7b7242a3836333a5312346
Alanjy Huang <alanhuang@Alanjys-MacBook-Air.local>
2025-12-03 22:19:37 -0600

    data path

--- verify_clip.sh
+++ verify_clip.sh
@@ -5,7 +5,7 @@
 
 CUDA_VISIBLE_DEVICES=7 python verify_clip_embeddings.py \
     --model_path output/1120/Kitchen/1_1_1__Dim64_16_FC9_16_KS0_1_5_RED1.2_low12_blk1_1_e300_b2_quant_M8_E6_lr0.001_cosine_0.1_1_0.1_L2_Size1.5_ENC_convnext_5,4,4,2,2_DEC_pshuffel_5,4,4,2,2_gelu1_1/model_best.pth \
-    --data_path data \
+    --data_path data/Kitchen \
     --vid Kitchen \
     --fc_dim 96 \
     --fc_hw 9_16 \
